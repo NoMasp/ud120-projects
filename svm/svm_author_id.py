76fcaf09c4f08e15da5f74bad2d6cb3ab9c6a338
Kinhiox <kinhiox@gmail.com>
2017-12-10 22:48:56 +0800

    add naive-bayes and svm code
    Signed-off-by: Kinhiox <kinhiox@gmail.com>

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -26,5 +26,34 @@ features_train, features_test, labels_train, labels_test = preprocess()
 ### your code goes here ###
 
 #########################################################
+from sklearn import svm
+from sklearn.metrics import accuracy_score
 
+clf = svm.SVC(kernel="rbf", C=10000.0)
 
+#features_train = features_train[:len(features_train)/100]
+#labels_train = labels_train[:len(labels_train)/100]
+
+# train
+t0 = time()
+clf.fit(features_train, labels_train)
+print "\ntraining time:", round(time()-t0, 3), "s"
+
+# predict
+t0 = time()
+pred = clf.predict(features_test)
+print "\npredicting time:", round(time()-t0, 3), "s"
+
+accuracy = accuracy_score(pred, labels_test)
+print('\naccuracy = {0}'.format(accuracy))
+
+print(pred[10])
+print pred[26]
+print(pred[50])
+
+count = 0
+for p in pred:
+    if p == 1:
+        count += 1
+
+print("count = {0}".format(count))
